9d11184a3ab84a928b5cd4fd318ef0ec3519e750
AMutaa <piusandru@gmail.com>
2019-03-11 21:37:10 -0400

    more useState

--- src/App.js
+++ src/App.js
@@ -11,10 +11,22 @@ const App = () => {
     <div className="main-wrapper">
       <h1>React Hooks</h1>
       <Toggle />
-      <h3>{name}</h3>
-      <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        formSubmit(name, setName);
+      }}>
+        <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+        <button>Submit</button>
+      </form>
+
     </div>
   );
 };
 
+
+// form Submit function to be called onSubmit, value can be used while setValue is going to clear the input field
+const formSubmit = (value, setValue) => {
+  console.log('email sent to ' + value + '!');
+  setValue('')
+}
 export default App;
