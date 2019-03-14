9204b488925ac8ed2db90c8696822d1f066a99e5
AMutaa <piusandru@gmail.com>
2019-03-13 22:25:14 -0400

    easily consume context via hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, createContext } from 'react';
 import Toggle from './Toggle'
 import { useTitleInput } from './hooks/useTitleInput'
+
+
+export const UserContext = createContext();
 
 const App = () => {
 
@@ -9,17 +12,22 @@ const App = () => {
   console.log('ref:', ref.current);
 
   return (
-    <div className="main-wrapper" ref={ref}>
-      <h1>React Hooks</h1>
-      <Toggle />
-      <form onSubmit={(e) => {
-        e.preventDefault();
-      }}>
-        <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
-        <button>Submit</button>
-      </form>
-
-    </div>
+    <UserContext.Provider
+      value={{
+        user: false
+      }}
+    >
+      <div className="main-wrapper" ref={ref}>
+        <h1>React Hooks</h1>
+        <Toggle />
+        <form onSubmit={(e) => {
+          e.preventDefault();
+        }}>
+          <input type="text" onChange={(e) => setName(e.target.value)} value={name} />
+          <button>Submit</button>
+        </form>
+      </div>
+    </UserContext.Provider>
   );
 };
 
